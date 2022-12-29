1a66a9b48b229c9df60037a5c5b34e1c4166123c
farhamapple@gmail.com <farhamapple@gmail.com>
2022-12-30 00:01:27 +0700

    Dart Beginner - Number

---
+++ bin/3_number.dart
@@ -0,0 +1,22 @@
+void main() {
+  int number1 = 10;
+  double number2 = 10.5;
+
+  print(number1);
+  print(number2);
+
+  // tipe data num
+  // jika kit ingin menggunakan tipe data number yang bisa int ataupun doble, kita bisa menggunakna tipe data enum;
+  num numberNum = 10;
+  print(numberNum);
+
+  numberNum = 15;
+  print(numberNum);
+
+  // Tipe dat aBoolean
+  bool finish = false;
+  print(finish);
+
+  finish = true;
+  print(finish);
+}

88bc72dd403994e802b44bd3d8e80568e6839eca
farhamapple@gmail.com <farhamapple@gmail.com>
2022-12-30 09:57:10 +0700

    Dart Beginner - Operator Perbandingan

---
+++ bin/8_operator_perbandingan.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  print('farham' == 'farham');
+}

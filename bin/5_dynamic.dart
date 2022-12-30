9d731c0312569831e2d6af4c5f67dc39c8c58cfb
farhamapple@gmail.com <farhamapple@gmail.com>
2022-12-30 08:33:50 +0700

    Dart Beginner - Dynamic

---
+++ bin/5_dynamic.dart
@@ -0,0 +1,16 @@
+// Tidak peduli tipe ada apaun itu
+
+void main() {
+  // Var tanpa declarasi juga bisa menjadi dynamic
+
+  var varDynamic;
+
+  dynamic variable = 100;
+  print(variable);
+
+  variable = true;
+  print(variable);
+
+  variable = 'Farham';
+  print(variable);
+}
